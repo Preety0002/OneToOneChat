55c728f23dbcf144ec3ac3a7e701cf8a1d5e2918
Preety0002 <prityarju0002@gmail.com>
2024-07-16 16:01:16 +0530

    updated

--- models/db.js
+++ models/db.js
@@ -1,5 +1,8 @@
 const mongoose=require("mongoose")
 
-mongoose.connect("mongodb://0.0.0.0/webpro").then(()=>
+mongoose.connect("mongodb+srv://arjuprity0002:123preetyy@cluster0.fsls1py.mongodb.net/onetoonechat?retryWrites=true&w=majority&appName=Cluster0").then(()=>
     console.log("db connected")
+
 )
+
+// mongodb://0.0.0.0/webpro
